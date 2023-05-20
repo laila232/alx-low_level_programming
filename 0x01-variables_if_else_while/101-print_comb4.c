4a34f7a403312780b041527838ca1a640decde30
laila232 <lailanajah76@gmail.com>
2023-05-19 22:30:55 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, e;
+for (i = 48; i <= 55; i++)
+{
+for (j = 49; j <= 56; j++)
+{
+for (e = 50; e <= 57; e++)
+{
+if (j > i && e > j)
+{
+putchar(i);
+putchar(j);
+putchar(e);
+if (i != 55 || j != 56 || e !=  57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
