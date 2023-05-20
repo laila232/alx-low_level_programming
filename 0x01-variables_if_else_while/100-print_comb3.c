a575eacf73b3b18ba73a9563db7f2b12f083895f
laila232 <lailanajah76@gmail.com>
2023-05-19 22:01:00 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0 to 9 with commas and spaces..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
