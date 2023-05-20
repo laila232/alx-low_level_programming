3584fef1ff8d32107d14ad028d80c7fd74da670f
laila232 <lailanajah76@gmail.com>
2023-05-19 19:26:44 -0700

    alphabets with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0 to 9 with commas and spaces..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+if (ch != 57)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
