075fe3fc26bd57d8072abf55cb076b133a7a35b3
laila232 <lailanajah76@gmail.com>
2023-05-25 17:09:36 -0700

    add 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ for (i = 0; i < 10; i++)
 for (j = 0; j <= 14; j++)
 {
 if (j > 9)
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
