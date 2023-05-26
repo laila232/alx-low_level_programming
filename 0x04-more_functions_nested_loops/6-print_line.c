802764f653ec4ea5aa8ae9e440d8fc6ebf315312
laila232 <lailanajah76@gmail.com>
2023-05-25 17:21:36 -0700

    add putchar

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,7 +7,11 @@
 void print_line(int n)
 {
 int i;
-if (n > 0)
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
 {
 for (i = 1; i <= n; i++)
 {
@@ -15,6 +19,4 @@ _putchar('_');
 _putchar('\n');
 }
 }
-else
-_putchar('\n');
 }
