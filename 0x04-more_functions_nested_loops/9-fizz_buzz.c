6d017f241edab57850ed30377d60f491a3e7c020
laila232 <lailanajah76@gmail.com>
2023-05-25 20:10:51 -0700

    add fizz buzz file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - prints numbers from 1 to 100.
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+}
