c4480e70dd2091c250efc4b48fcb418f40ff1b3f
laila232 <lailanajah76@gmail.com>
2023-05-25 21:46:57 -0700

    add fizz buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,17 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("Fizz ");
+printf("FizzBuzz ");
 }
 else if (i % 5 == 0)
 {
 printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
 }
 else
 {
